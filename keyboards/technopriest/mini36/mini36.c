e9ba718108906b8676ec516477c9348ae54b4602
supremetechnopriest <randylebeau@gmail.com>
2023-10-03 17:36:16 -0700

    ipc

--- keyboards/technopriest/mini36/mini36.c
+++ keyboards/technopriest/mini36/mini36.c
@@ -430,3 +430,44 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 #endif
+
+layer_state_t default_layer_set_user (layer_state_t state) {
+  int16_t layer = _QWERTY;
+
+  if (IS_LAYER_ON_STATE(state, _QWERTY)) {
+    layer = _QWERTY;
+  } else if (IS_LAYER_ON_STATE(state, _HALMAK)) {
+    layer = _HALMAK;
+  }
+
+  tap_code16(KC_F13 + layer);
+  return state;
+}
+
+layer_state_t layer_state_set_user (layer_state_t state) {
+  int16_t layer = get_highest_layer(default_layer_state);
+  if (IS_LAYER_ON_STATE(state, _QWERTY)) {
+    layer = get_highest_layer(default_layer_state);
+  } else if (IS_LAYER_ON_STATE(state, _HALMAK)) {
+    layer = get_highest_layer(default_layer_state);
+  } else if (IS_LAYER_ON_STATE(state, _NAV)) {
+    layer = _NAV;
+  } else if (IS_LAYER_ON_STATE(state, _MOUSE)) {
+    layer = _MOUSE;
+  } else if (IS_LAYER_ON_STATE(state, _BUTTON)) {
+    layer = _BUTTON;
+  } else if (IS_LAYER_ON_STATE(state, _MEDIA)) {
+    layer = _MEDIA;
+  } else if (IS_LAYER_ON_STATE(state, _NUMBER)) {
+    layer = _NUMBER;
+  } else if (IS_LAYER_ON_STATE(state, _SYMBOL)) {
+    layer = _SYMBOL;
+  } else if (IS_LAYER_ON_STATE(state, _FUNCTION)) {
+    layer = _FUNCTION;
+  } else if (IS_LAYER_ON_STATE(state, _CONTROL)) {
+    layer = _CONTROL;
+  }
+
+  tap_code16(KC_F13 + layer);
+  return state;
+}
